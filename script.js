5681388e8a2de6d16cea1a145aeb693ef17c7a1a
shegol2020 <shegol2007@gmail.com>
2024-04-04 15:07:14 +0300

    fix: dark mode switcher

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 //theme change
 
 const themeButtonsContainer = document.querySelector(".theme-switcher");
+const lightThemeButton = document.querySelector(".theme-button-light");
+const darkThemeButton = document.querySelector(".theme-button-dark");
+
 themeButtonsContainer.addEventListener("click", (e) => {
-    const buttons = themeButtonsContainer.querySelectorAll("button");
-    buttons.forEach(button => {
-        button.classList.add("active");
-    });
-    if(e.target.classList.contains("active")){
-        e.target.classList.remove("active");
+    if(e.target.className === "theme-button-dark"){
+        document.body.classList.add("dark");
+        lightThemeButton.classList.remove("active");
+        darkThemeButton.classList.add("active");
+    } else {
+        document.body.classList.remove("dark");
+        lightThemeButton.classList.add("active");
+        darkThemeButton.classList.remove("active");
     }
-    e.target.className === "theme-button-dark"
-        ? document.body.classList.add("dark")
-        : document.body.classList.remove("dark");
 })
+
