bd7420948433ac22d4df9fb1be724ed356054fb1
shegol2020 <shegol2007@gmail.com>
2024-04-04 16:38:58 +0300

    feat: font change and layout change buttons

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
+const switchButtons = (makeActive, removeActive) => {
+    removeActive.classList.remove("active");
+    makeActive.classList.add("active");
+}
+
 //theme change
 
 const themeButtonsContainer = document.querySelector(".theme-switcher");
-const lightThemeButton = document.querySelector(".theme-button-light");
-const darkThemeButton = document.querySelector(".theme-button-dark");
+const lightThemeButton = themeButtonsContainer.querySelector(".theme-button-light");
+const darkThemeButton = themeButtonsContainer.querySelector(".theme-button-dark");
 
 themeButtonsContainer.addEventListener("click", (e) => {
     if(e.target.className === "theme-button-dark"){
         document.body.classList.add("dark");
-        lightThemeButton.classList.remove("active");
-        darkThemeButton.classList.add("active");
+        switchButtons(darkThemeButton, lightThemeButton);
     } else {
         document.body.classList.remove("dark");
-        lightThemeButton.classList.add("active");
-        darkThemeButton.classList.remove("active");
+        switchButtons(lightThemeButton, darkThemeButton);
+    }
+})
+
+//font change
+const fontsSwitcherContainer = document.querySelector(".fonts");
+const serifButton = fontsSwitcherContainer.querySelector(".font-button-serif");
+const sansSerifButton = fontsSwitcherContainer.querySelector(".font-button-sans-serif");
+
+fontsSwitcherContainer.addEventListener("click", (e) => {
+    if(e.target.className === "font-button-serif"){
+        document.body.classList.add("serif");
+        switchButtons(sansSerifButton, serifButton)
+    } else {
+        document.body.classList.remove("serif");
+        switchButtons(sansSerifButton, serifButton);
+    }
+})
+
+//article wrap
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("more")){
+        let currentArticle = e.target.parentElement.parentElement;
+        currentArticle.classList.remove("short");
+    }
+})
+
+//cards list and columns layout
+
+const cardsList = document.querySelector(".cards");
+const cardsSwitchButtonsContainer = document.querySelector(".card-view-buttons");
+const buttonGridView = cardsSwitchButtonsContainer.querySelector(".card-view-button-grid");
+const buttonListView = cardsSwitchButtonsContainer.querySelector(".card-view-button-list");
+
+cardsSwitchButtonsContainer.addEventListener("click", (e) => {
+    if(e.target.className === "card-view-button-list"){
+        cardsList.classList.add("list");
+        switchButtons(buttonListView, buttonGridView)
+    } else {
+        cardsList.classList.remove("list");
+        switchButtons(buttonGridView, buttonListView);
     }
 })
 
